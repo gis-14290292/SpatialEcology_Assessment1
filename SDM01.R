685a7f1ad9050882218b7393de888061eebdd1d4
gis-14290292 <ziyao.zhao@student.manchester.ac.uk>
2026-04-02 10:10:31 +0100

    Reclassify land cover and derive broadleaf neighbourhood cover

--- SDM01.R
+++ SDM01.R
@@ -67,3 +67,76 @@ melesmelesFin=melesmeles.sp[scot,]
 LCM=crop(LCM,scot,mask=TRUE)
 plot(LCM)
 plot(melesmelesFin$geometry,add=T)
+
+#access levels of the raster by treating them as categorical data ('factors' in R)
+LCM=as.factor(LCM$LCMUK_1)
+
+#create an vector object called reclass
+reclass = c(0,1,rep(0,20))
+
+# combine with the LCM categories into a matrix of old and new values.
+RCmatrix=cbind(levels(LCM)[[1]],reclass)
+
+RCmatrix=RCmatrix[,2:3]
+
+#apply function to make sure new columns are numeric (here the "2" specifies that we want to apply the as.numeric function to columns, where "1" would have specified rows)
+RCmatrix=apply(RCmatrix,2,FUN=as.numeric)
+#Use the classify() function to asssign new values to LCM with our reclassification matrix
+
+broadleaf=classify(LCM, RCmatrix)
+
+#neighbourhood weights matrix to sum all available resources for each cell
+
+#get number of picels needed to cover the 1800 metre radius 
+nPix=round(900/res(LCM)[1])
+
+#next, you need to double this number
+nPix=(nPix*2)+1
+
+#buiild weights matrix
+weightsMatrix=matrix(1:nPix^2,nrow=nPix,ncol=nPix)
+
+#get focal cell 
+x=ceiling(ncol(weightsMatrix)/2)
+y=ceiling(nrow(weightsMatrix)/2)
+
+
+focalCell=weightsMatrix[x,y]
+
+indFocal=which(weightsMatrix==focalCell,arr.ind = TRUE)
+
+#compute distances
+distances=list()
+
+for(i in 1:nPix^2){
+  ind.i=which(weightsMatrix==i,arr.ind=T)
+  diffX=abs(ind.i[1,1]-indFocal[1,1])*res(LCM)[1]
+  diffY=abs(ind.i[1,2]-indFocal[1,2])*res(LCM)[1]
+  
+  dist.i=sqrt(diffX^2+diffY^2)
+  distances[[i]]=dist.i
+  
+}
+
+#add distance values to the weights matrix
+weightsMatrix[]=unlist(distances)
+
+#set cells outside search radius to NA
+weightsMatrix[weightsMatrix>900]=NA
+
+#plot weights matrix
+plot(rast(weightsMatrix))
+
+#normalise the weights matrix by dividing all cell values by the number of cells. 
+weightsMatrixNorm=weightsMatrix
+weightsMatrixNorm[!is.na(weightsMatrixNorm)]=1/length(weightsMatrixNorm[!is.na(weightsMatrixNorm)])
+
+#test to see for yourself
+sum(weightsMatrixNorm,na.rm=T)
+
+plot(rast(weightsMatrixNorm))
+
+#sum neighbourhood values from all surrounding cells
+lcm_wood_900=focal(broadleaf,w=weightsMatrixNorm,fun="sum")
+
+plot(lcm_wood_900)
