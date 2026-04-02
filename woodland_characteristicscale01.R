93244c92751f89120ae321423c73e170e26e11de
gis-14290292 <ziyao.zhao@student.manchester.ac.uk>
2026-04-02 09:39:24 +0100

    Test multiple buffer radii with GLM and identify optimum scale

--- woodland_characteristicscale01.R
+++ woodland_characteristicscale01.R
@@ -160,3 +160,64 @@ landBuffer <- function(speciesData, r){
   # return the result
   return(percentcover)                                       
 }
+radii<-seq(100,2000,by=100)
+resList=list()
+
+for(i in radii){
+  res.i=landBuffer(speciesData=melesmelesSF,r=i)
+  res.i
+  resList[[i/100]]=res.i
+  print(i)
+}
+#collect all results together
+resFin=do.call("cbind",resList)
+
+#convert to data frame
+glmData=data.frame(resFin)
+
+#assign more intuitive column names
+colnames(glmData)=paste("radius",radii,sep="")
+#inspect
+
+head(glmData)
+#add in the presences data
+glmData$Pres<-melesmelesData$Pres
+
+
+head(glmData)
+
+#make an empty data frame to then add a sequence of results from glms (general linear models) that test the relationship between broadleaf cover and red squirrel presence for the different buffer sizes
+
+#init empty data frame
+glmRes=data.frame(radius=NA,loglikelihood=NA)
+
+#for loop to iterate over radius values and run a general linear model with glm()
+for(i in radii){
+  
+  #build the model formula with format "response variable ~ explanatory variable, family, data"
+  #Here "binomial" is the error distribution normally used for a binary outcome (e.g. 0, 1) 
+  n.i=paste0("Pres~","radius",i,sep ="")
+  
+  glm.i=glm(formula(n.i),family = "binomial",data = glmData)
+  
+  ll.i=as.numeric(logLik(glm.i))
+  
+  glmRes=rbind(glmRes,c(i,ll.i))
+  
+}
+
+#inspect
+head(glmRes)
+
+plot(glmRes$radius, glmRes$loglikelihood, type = "b", frame = FALSE, pch = 19, 
+     col = "red", xlab = "buffer", ylab = "logLik")
+
+# finding the max loglikelihood then determine the optimum buffer size
+#remove the NAs in the first row
+glmRes=glmRes[!is.na(glmRes),]
+
+#use the which.max function to subset the dataframe to the just the row containing the max log likelihood value
+opt<-glmRes[which.max(glmRes$loglikelihood),]
+#print
+opt
+
