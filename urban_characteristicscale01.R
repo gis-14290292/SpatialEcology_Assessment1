65877b868f25026abd1e652bd917b96c434d14e7
gis-14290292 <ziyao.zhao@student.manchester.ac.uk>
2026-04-02 11:13:00 +0100

    Generate pseudo-absence points and explore land-cover frequencies

--- urban_characteristicscale01.R
+++ urban_characteristicscale01.R
@@ -47,3 +47,32 @@ LCM <- crop(LCM$LCMUK_1, extent.new)
 
 plot(LCM)
 plot(melesmelesFin,add=TRUE)
+
+
+#Generate pseudo-absence points
+set.seed(11)
+back.xy <- spatSample(LCM, size=1000,as.points=TRUE) 
+#create a spatialPoints layer from the back.xy matrix
+plot(melesmelesFin,add=T)
+plot(back.xy,add=TRUE, col='red')
+
+#extract land cover values at background and presence points
+eA<-extract(LCM,back.xy)
+eP<-extract(LCM,melesmelesFin)
+
+#calculate point frequency per landcover category (headings are landcover category codes - see LCM documentation for descriptions)
+table(eA[,2])
+table(eP[,2])
+
+par(mfrow=c(1,2))
+
+hist(eA[,2],freq=FALSE,breaks=c(0:21),xlim=c(0,21),ylim=c(0,1))
+hist(eP[,2],freq=FALSE,breaks=c(0:21),xlim=c(0,21),ylim=c(0,1))
+
+hist(eA[,2],freq=FALSE,breaks=c(0:21),xlim=c(0,21),ylim=c(0,0.4))
+hist(eP[,2],freq=FALSE,breaks=c(0:21),xlim=c(0,21),ylim=c(0,0.4))
+
+
+
+
+
