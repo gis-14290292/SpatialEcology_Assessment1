66ca449e08cd1a0fd1db07a25c1b05a7c89a082c
gis-14290292 <ziyao.zhao@student.manchester.ac.uk>
2026-04-02 11:13:30 +0100

    Create presence-absence dataset and reclassify urban land cover

--- urban_characteristicscale01.R
+++ urban_characteristicscale01.R
@@ -73,6 +73,46 @@ hist(eA[,2],freq=FALSE,breaks=c(0:21),xlim=c(0,21),ylim=c(0,0.4))
 hist(eP[,2],freq=FALSE,breaks=c(0:21),xlim=c(0,21),ylim=c(0,0.4))
 
 
+#Build presence/absence dataset
+Abs<-data.frame(crds(back.xy),Pres=0)
+head(Abs)
+
+Pres<-data.frame(crds(melesmelesFin),Pres=1)
+head(Pres)
+
+#bind the two data frames by row (both dataframes have the same column headings)
+melesmelesData<-rbind(Pres,Abs)
+#inspect
+head(melesmelesData)
+
+#convert to sf object for buffering
+melesmelesSF=st_as_sf(melesmelesData,coords=c("x","y"),crs="EPSG:27700")
+
+#access levels of the raster by treating them as categorical data ('factors' in R)
+#Reclassify
+
+#treat raster as categorical data
+LCM <- as.factor(LCM)
+
+#default = 0 for all classes
+reclass <- rep(0, nrow(levels(LCM)[[1]]))
+
+#choose urban classes
+urban_codes <- c(20, 21)
+
+#set urban/suburban to 1
+reclass[levels(LCM)[[1]]$ID %in% urban_codes] <- 1
+
+# create reclassification matrix
+RCmatrix <- cbind(levels(LCM)[[1]]$ID, reclass)
+RCmatrix <- apply(RCmatrix, 2, as.numeric)
+
+# classify raster
+urban <- classify(LCM, RCmatrix)
+
+plot(urban)
+plot(melesmelesFin, add = TRUE)
+
 
 
 
