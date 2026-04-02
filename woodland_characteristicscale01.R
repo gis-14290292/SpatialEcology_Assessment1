88469a7f00bb799e0691a64e1e17e890ac59f253
gis-14290292 <ziyao.zhao@student.manchester.ac.uk>
2026-04-02 11:00:47 +0100

    ample background points and explore land-cover frequencies

--- woodland_characteristicscale01.R
+++ woodland_characteristicscale01.R
@@ -53,3 +53,20 @@ back.xy <- spatSample(LCM, size=1000,as.points=TRUE)
 plot(LCM)
 plot(melesmelesFin,add=T)
 plot(back.xy,add=TRUE, col='red')
+
+#Extract values from the background points
+eA<-extract(LCM,back.xy)
+eP<-extract(LCM,melesmelesFin)
+
+#calculate point frequency per landcover category (headings are landcover category codes - see LCM documentation for descriptions)
+table(eA[,2])
+table(eP[,2])
+
+par(mfrow=c(1,2))
+
+hist(eA[,2],freq=FALSE,breaks=c(0:21),xlim=c(0,21),ylim=c(0,1))
+hist(eP[,2],freq=FALSE,breaks=c(0:21),xlim=c(0,21),ylim=c(0,1))
+
+hist(eA[,2],freq=FALSE,breaks=c(0:21),xlim=c(0,21),ylim=c(0,0.4))
+hist(eP[,2],freq=FALSE,breaks=c(0:21),xlim=c(0,21),ylim=c(0,0.4))
+
